ea1cc8b811c1660cc399e74b9a29520785029a72
imrankhan <imran.khan@marginedge.com>
2023-08-05 19:14:44 +0600

    add feature assets

--- src/Utils/customStyles.ts
+++ src/Utils/customStyles.ts
@@ -1,2 +1,3 @@
-export const sectionTitle = "font-poppins text-3xl font-semibold pb-4";
-export const sectionDescription = "font-poppins text-md";
+export const sectionTitle = "font-poppins text-3xl font-semibold my-4";
+export const sectionDescription = "text-sm font-poppins text-md";
+export const pageTitle = "text-4xl font-bold my-8";
