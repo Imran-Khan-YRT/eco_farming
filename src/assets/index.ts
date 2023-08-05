dbb51da8c8ebe72f1710819c031d82c5fdba9fe0
imrankhan <imran.khan@marginedge.com>
2023-08-05 19:21:03 +0600

    Revert "Revert "add feature assets""
    This reverts commit 08296190794c072c7e411016b7ba81edbd97e9d9.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -22,8 +22,16 @@ import footer_linkedIn from "./Footer/footer_linkedIn.png";
 import footer_twitter from "./Footer/footer_twitter.png";
 import footer_youtube from "./Footer/footer_youtube.png";
 
+// feature images
+import feature_iphone13 from "./features/feature_iphone13.png";
+import feature_playIcon from "./features/feature_playIcon.png";
+import image1 from "./features/image1.png";
+import image2 from "./features/image2.png";
+
 export const footerIcons = [footer_facebook, footer_instagram, footer_linkedIn, footer_twitter, footer_youtube];
 export const footerButtons = [footer_app_store, footer_google_play];
 
 export { app_logo, home_image1, home_image2, home_image3, menuIcon };
 export { contact_logo, contact_call, contact_email, contact_facebook, contact_linkedIn, conact_location, contact_twitter };
+
+export { feature_iphone13, feature_playIcon, image1, image2 };
