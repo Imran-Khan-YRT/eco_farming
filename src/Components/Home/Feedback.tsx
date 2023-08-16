d68caa027c34c3a97142c853b8a945d8db3dbd46
imrankhan <imran.khan@marginedge.com>
2023-08-16 23:34:22 +0600

    slight modification in feedback

--- src/Components/Home/Feedback.tsx
+++ src/Components/Home/Feedback.tsx
@@ -34,12 +34,12 @@ const FeedbackComponent: React.FC<FeedbackProps> = ({ feedback }) => {
     // some animation in future
     return (
       <Container margined={false} paddinged={false}>
-        <FlexWrapper className="pt-10 justify-center text-center px-1">
+        <FlexWrapper className="pt-10 justify-center text-center">
           <img className={"w-6 h-6 cursor-pointer"} src={left_arrow} alt={"left_arrow"} onClick={handlePrevClick} />
           <Container type="card" className="lg:w-[32%] md:w-[45%] sm:w-[55%] w-[70%] min-w-[250px] rounded relative" backgroundColor="bg-[#F2FFF6]">
             <img className={"absolute sm:-top-12  -top-5 left-[40%] md:w-20 sm:w-16 w-12  md:h-20 sm:h-16 h-12 rounded-full  z-20"} src={person} alt={"person"} />
             <FlexWrapper flexDirection="flex-col pt-4">
-              <div className={`${listText_3} max-h-[200px] overflow-scroll`}>{currentFeedback.comment}</div>
+              <div className={`${listText_3} h-[170px] overflow-scroll`}>{currentFeedback.comment}</div>
               <div>{currentFeedback.name}</div>
               <div className={`${captionText}`}>{currentFeedback.country}</div>
             </FlexWrapper>
