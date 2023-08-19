cbb6b91d6441a6781d2a8e0d7c8b057a101dd290
imrankhan <imran.khan@marginedge.com>
2023-08-19 16:27:33 +0600

    fix refresh home link triggered issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import GetRoutes from "./Utils/GetRoutes/GetRoutes";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
-  const [activeLink, setActiveLink] = useState("home");
+  const [activeLink, setActiveLink] = useState<string>("home");
+  useEffect(() => {
+    const storedActiveLink = window.localStorage.getItem("activeLink");
+    if (storedActiveLink !== null) {
+      setActiveLink(storedActiveLink);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("activeLink", activeLink);
+  }, [activeLink]);
+
   return (
     <div className="w-full">
       <Header activeLink={activeLink} setActiveLink={setActiveLink} />
